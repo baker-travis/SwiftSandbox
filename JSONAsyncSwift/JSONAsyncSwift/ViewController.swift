a79d27a27b7a853550715e5bdac3da4c57241d31
Travis Baker <baker.travis.w@gmail.com>
2014-10-28 10:24:07 -0600

    Fixed layout issues.

---
+++ JSONAsyncSwift/JSONAsyncSwift/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  JSONAsyncSwift
+//
+//  Created by Travis Baker on 10/28/14.
+//  Copyright (c) 2014 Travis Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func post(params : Dictionary<String, String>, url : String, method : String, postCompleted : (succeeded: Bool, msg: String) -> ()) {
+        let theURL = NSURL(string: url)
+        var request = NSMutableURLRequest(URL: theURL!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = method
+        
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            var msg = "No message"
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+                postCompleted(succeeded: false, msg: "Error")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    if let success = parseJSON["success"] as? Bool {
+                        println("Succes: \(success)")
+                        postCompleted(succeeded: success, msg: "Logged in.")
+                    }
+                    return
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: \(jsonStr)")
+                    postCompleted(succeeded: false, msg: "Error")
+                }
+            }
+        })
+        
+        task.resume()
+    }
+    
+    @IBAction func getAPost(sender: AnyObject) {
+        post(["":""], url: "http://api.cbssports.com/fantasy/sports?version=3.0", method: "GET", postCompleted: { (succeeded: Bool, msg: String) -> () in
+            NSLog(msg)
+        })
+    }
+
+}
+
