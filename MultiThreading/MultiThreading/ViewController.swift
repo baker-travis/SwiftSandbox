fdfda21b8ee1988d575be5bed724c334c1d01e11
Travis Baker <baker.travis.w@gmail.com>
2014-11-10 10:31:11 -0700

    Cleaned up code slightly.

--- MultiThreading/MultiThreading/ViewController.swift
+++ MultiThreading/MultiThreading/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var backgroundQueue: NSOperationQueue? = nil
+    var backgroundQueue: NSOperationQueue?
 
     @IBOutlet weak var myLabel: UILabel!
     
